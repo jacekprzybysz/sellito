e108d462ab037f27360aee825b53946b5c098ffe
Jacek Przybysz <silvuss@op.pl>
2017-05-05 21:01:31 +0200

    add categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,5 @@
+class CategoriesController < ApplicationController
+  def new
+    #TODO:
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homepage#index'
 
+  resources :categories
+
   get '/about', to: 'homepage#about'
   get '/cookies', to: 'homepage#cookies'
 end
