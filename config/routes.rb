3405ed685668776a5e734661872f5ee415828f17
Jacek Przybysz <silvuss@op.pl>
2017-05-13 22:36:42 +0200

    add post resources to project

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homepage#index'
 
+  resources :posts
+
   resources :categories
+
 
   get '/about', to: 'homepage#about'
   get '/cookies', to: 'homepage#cookies'
